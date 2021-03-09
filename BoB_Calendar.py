f45e54d1af189b8b71fbb3bcb61ab454fcf78cc2
ugonfor <ryuhyogon@gmail.com>
2021-03-09 16:47:56 +0900

    Update BoB_Calendar.py

--- BoB_Calendar.py
+++ BoB_Calendar.py
@@ -101,7 +101,6 @@ def Copy_To_My_Calendar():
         # if name start with "[공]" append to ret
         if event['summary'][:3] == "[공]":
             #print(start, event['summary'])
-            target_events.append(copy.deepcopy(event))
             event.pop('iCalUID')
             event.pop('sequence')
             event.pop('htmlLink')
@@ -112,17 +111,18 @@ def Copy_To_My_Calendar():
             event.pop('created')
             event.pop('updated')
             event.pop('organizer')
+            target_events.append(copy.deepcopy(event))
             #print(event)
 
     for event in target_events:
         try:
             events_inserted = service.events().insert(calendarId='neplqpk76mi4ahtrmu2ru29g44@group.calendar.google.com', body=event).execute()
+            print(events_inserted)
         except Exception as e:
             if "identifier already exists" in str(e):
                 print("Already Exists")
             else:
                 print(e)
-        print(events_inserted)
 
 def Add_Alert_To_Events():
     creds = None
@@ -168,7 +168,46 @@ def Add_Alert_To_Events():
         events_result = service.events().update(calendarId='neplqpk76mi4ahtrmu2ru29g44@group.calendar.google.com', eventId=event.get('id'), body=event_with_alert).execute()
         print(events_result)
 
+def Delete_Events():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    time = '2021-01-03T00:00:00.000000Z'
+    print('Getting the upcoming 300 events')
+
+    #change the calendarId to target calendar's ID
+    events_result = service.events().list(calendarId='neplqpk76mi4ahtrmu2ru29g44@group.calendar.google.com', timeMin=time,
+                                        maxResults=300, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    for event in events:
+        events_result = service.events().delete(calendarId='neplqpk76mi4ahtrmu2ru29g44@group.calendar.google.com', eventId=event.get('id')).execute()
+        print(events_result)
+
+
 if __name__ == '__main__':
-    #main('neplqpk76mi4ahtrmu2ru29g44@group.calendar.google.com')
+    Delete_Events()
+    Copy_To_My_Calendar()
     Add_Alert_To_Events()
     print("DONE")
